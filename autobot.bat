bac3fbde2d742680e8e668a40f113c3e3d7d0df2
Rohan Pinto <168577489+rohannpinto@users.noreply.github.com>
2026-07-19 14:17:59 -0400

    upgrading auto start

--- autobot.bat
+++ autobot.bat
@@ -1,2 +1,10 @@
-cd C:\Users\Rohan\Documents\Coding Projects\HarambeBot
+@echo off
+set KENKU_PATH=C:\Users\Rohan\AppData\Local\kenku-fm\kenku-fm.exe
+set DEBUG_PORT=9222
+
+echo Starting Kenku FM with remote debugging...
+start "" "%KENKU_PATH%" --remote-debugging-port=%DEBUG_PORT%
+
+echo Starting HarambeBot...
+cd "C:\Users\Rohan\Documents\Coding Projects\HarambeBot"
 node index.js
